3c0f1f73fe0afd75eca5d179bc82ef684700f122
Rian Neufelt <RNeufelt@live.ca>
2014-06-12 21:59:37 -0700

    test

--- testlol/StateMachine/StateMachine.cs
+++ testlol/StateMachine/StateMachine.cs
@@ -8,7 +8,7 @@ namespace testlol.StateMachine
     {
         public bool Running { get; set; }
         public bool Resume { get; set; }
-        private Stack<GameState> states = new Stack<GameState>();
+        private Stack<GameState> _states = new Stack<GameState>();
 
         public StateMachine()
         {

dba39db723f1a686951894c45ef49ef04a340eb0
Rian Neufelt <RNeufelt@live.ca>
2014-06-17 04:01:03 -0700

    tilemap test stuff

---
+++ testlol/World/Map.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.IO;
+using SFML.Window;
+
+namespace testlol.World
+{
+    public struct Map
+    {
+        private readonly int[,] _map;
+        public int X { get { return _map.GetLength(1); } }
+        public int Y { get { return _map.GetLength(0); } }
+
+        public Map(Vector2u size)
+        {
+            _map = new int[size.Y,size.X];
+        }
+
+        public Map(int x, int y)
+        {
+            _map = new int[y, x];
+        }
+
+        public Map(int[,] a)
+        {
+            _map = a;
+        }
+
+        public int this[int a, int b]
+        {
+            get { return _map[a, b]; }
+            set { _map[a, b] = value; }
+        }
+
+        public static implicit operator Map(int[,] a)
+        {
+            return new Map(a);
+        }
+
+        public static implicit operator int[,](Map a)
+        {
+            return a._map;
+        }
+
+        public static Map LoadMap(string s)
+        {
+            Map m = null;
+            using (BinaryReader br = new BinaryReader(new FileStream(s, FileMode.Open)))
+            {
+                int x = br.ReadInt32();
+                int y = br.ReadInt32();
+                m = new Map(x,y);
+                for (int i = 0; i < y; i++)
+                {
+                    for (int j = 0; j < x; j++)
+                    {
+                        m[i, j] = br.ReadInt32();
+                    }
+                }
+            }
+            return m;
+        }
+
+        public void WriteMap(string s)
+        {
+            using (BinaryWriter bw = new BinaryWriter(new FileStream(s, FileMode.Create)))
+            {
+                bw.Write(X);
+                bw.Write(Y);
+                
+                for (int i = 0; i < Y; i++)
+                {
+                    for (int j = 0; j < X; j++)
+                    {
+                        bw.Write(_map[i,j]);
+                    }
+                }
+                bw.Flush();
+            }
+            
+        }
+
+    }
+}
---
+++ testlol/World/TileMap.cs
@@ -0,0 +1,56 @@
+﻿using SFML.Graphics;
+using SFML.Window;
+
+namespace testlol.World
+{
+    public class TileMap : Transformable, Drawable
+    {
+        private readonly VertexArray _vertexArray = new VertexArray(PrimitiveType.Quads);
+        private readonly Texture _texture;
+        private readonly Vector2u _tileSize;
+        private readonly Map _map;
+
+        public TileMap(Texture texture, Vector2u tileSize, Map map)
+        {
+            _texture = texture;
+            _tileSize = tileSize;
+            _map = map;
+            Construct();
+        }
+
+        private void Construct()
+        {
+            for (int i = 0; i < _map.Y; i++)
+            {
+                for (int j = 0; j < _map.X; j++)
+                {
+                    int tile = _map[i, j];
+                    int tu = tile % ((int)(_texture.Size.X / _tileSize.X));
+                    int tv = tile / ((int)(_texture.Size.X / _tileSize.X));
+
+                    Vertex quad0 = new Vertex(new Vector2f( j * _tileSize.X,       i * _tileSize.Y));
+                    Vertex quad1 = new Vertex(new Vector2f((j + 1) * _tileSize.X,  i * _tileSize.Y));
+                    Vertex quad2 = new Vertex(new Vector2f((j + 1) * _tileSize.X, (i + 1) * _tileSize.Y));
+                    Vertex quad3 = new Vertex(new Vector2f( j * _tileSize.X,      (i + 1) * _tileSize.Y));
+
+                    quad0.TexCoords = new Vector2f(tu * _tileSize.X, tv * _tileSize.Y);
+                    quad1.TexCoords = new Vector2f((tu + 1) * _tileSize.X, tv * _tileSize.Y);
+                    quad2.TexCoords = new Vector2f((tu + 1) * _tileSize.X, (tv + 1) * _tileSize.Y);
+                    quad3.TexCoords = new Vector2f(tu * _tileSize.X, (tv + 1) * _tileSize.Y);
+
+                    _vertexArray.Append(quad0);
+                    _vertexArray.Append(quad1);
+                    _vertexArray.Append(quad2);
+                    _vertexArray.Append(quad3);
+                }
+            }
+        }
+
+        public void Draw(RenderTarget target, RenderStates states)
+        {
+            states.Transform = Transform;
+            states.Texture = _texture;
+            target.Draw(_vertexArray, states);
+        }
+    }
+}
